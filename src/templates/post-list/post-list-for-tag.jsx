c47fa0630b7dc82eb75231bcb1bf3fc67b9af014
Alvin <bestkun0401@gmail.com>
2020-12-18 19:48:45 +0800

    [feature] Add excepts

---
+++ src/templates/post-list/post-list-for-tag.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import PostListTemplate from './index'
+
+const PostForTag = ({
+  data: { allMarkdownRemark: { edges: posts } },
+  pageContext: { currentPage, numPages, tag },
+}) => {
+  const props = {
+    title: '標籤列表',
+    pathname: {
+      firstPagePath: `/tags/${tag}`,
+      restPagePath: `/tags/${tag}`,
+    },
+    posts,
+    currentPage,
+    numPages,
+  }
+  return <PostListTemplate {...props} />
+}
+
+export default PostForTag
+
+export const pageQuery = graphql`
+  query ($skip: Int!, $limit: Int!, $tag: String) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+      limit: $limit
+      skip: $skip
+  ) {
+      edges {
+        node {
+          excerpt(truncate: true, pruneLength: 120)
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            tags
+          }
+        }
+      }
+    }
+  }
+`
